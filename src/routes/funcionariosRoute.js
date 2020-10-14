4afb3b48e34b2f5f4c364571c742cc10fe7f97d2
TalitaFraga <talitad.fraga@gmail.com>
2020-10-14 17:50:23 -0300

    finalizado

--- src/routes/funcionariosRoute.js
+++ src/routes/funcionariosRoute.js
@@ -7,5 +7,7 @@ router.get("/funcionarios", controller.getAll)
 router.get("/:id", controller.getAgeById)
 router.post("/", controller.postFuncionarios)
 router.delete("/:id", controller.deleteFuncionarios)
+router.put("/:id", controller.putFuncionarios)
+router.patch("/:id", controller.patchFuncionarios)
 
 module.exports = router
